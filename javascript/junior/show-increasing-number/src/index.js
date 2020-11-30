dd3d670e167ebea800888500bab05e6ec7b460ee
4rontender <4rontender@gmail.com>
2020-11-30 16:48:00 +0500

    create function showNumber

--- javascript/junior/show-increasing-number/src/index.js
+++ javascript/junior/show-increasing-number/src/index.js
@@ -1,3 +1,17 @@
-let app = document.querySelector('#app')
+// let app = document.querySelector('#app');
 
-app.innerHTML = '<h2>Welcome to show-increasing-number</h2>'
+// app.innerHTML = '<h2>Welcome to show-increasing-number</h2>';
+
+const showNumber = () => {
+  let number = 0;
+
+  setTimeout(function output() {
+    console.log(number++);
+
+    if (number > 0) {
+      setTimeout(output, number * 1000);
+    }
+  }, 0);
+};
+
+showNumber();
