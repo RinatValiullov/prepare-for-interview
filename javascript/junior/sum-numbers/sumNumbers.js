dabead88a4a5858222703f1a058fca6185977267
Rinat Valiullov <4rontender@gmail.com>
2021-07-23 14:12:25 +0500

    fix: add notes in function and change a name

--- javascript/junior/sum-numbers/sumNumbers.js
+++ javascript/junior/sum-numbers/sumNumbers.js
@@ -1,4 +1,6 @@
-const sumTo = (number) => {
+// with for loop
+
+const sumTo_fl = (number) => {
   let sum = 0;
 
   for (let i = 1; i <= number; i++) {
@@ -12,6 +14,6 @@ const num1 = 3;
 const num2 = 1;
 const num3 = 100;
 
-const results = [sumTo(num1), sumTo(num2), sumTo(num3)];
+const results = [sumTo_fl(num1), sumTo_fl(num2), sumTo_fl(num3)];
 
 console.log(results);
