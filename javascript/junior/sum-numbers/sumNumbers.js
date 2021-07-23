71b03517d4f3da9c79db497b37f1dcc6e1c9ae32
Rinat Valiullov <4rontender@gmail.com>
2021-07-23 14:33:45 +0500

    update "sum-number" task
    use recursion

--- javascript/junior/sum-numbers/sumNumbers.js
+++ javascript/junior/sum-numbers/sumNumbers.js
@@ -10,10 +10,32 @@ const sumTo_fl = (number) => {
   return sum;
 };
 
-const num1 = 3;
-const num2 = 1;
-const num3 = 100;
+const num1_fl = 3;
+const num2_fl = 1;
+const num3_fl = 100;
 
-const results = [sumTo_fl(num1), sumTo_fl(num2), sumTo_fl(num3)];
+const results_fl = [sumTo_fl(num1_fl), sumTo_fl(num2_fl), sumTo_fl(num3_fl)];
 
-console.log(results);
+console.log(`With for loop: ${results_fl}`);
+
+// with recursion
+
+const sumTo_re = (number) => {
+  let sum = 0;
+
+  if (number == 0 || number == 1) {
+    sum += number;
+    return sum;
+  } else {
+    sum = number + sumTo_re(number - 1);
+    return sum;
+  }
+};
+
+const num1_re = 3;
+const num2_re = 1;
+const num3_re = 100;
+
+const results_re = [sumTo_re(num1_re), sumTo_re(num2_re), sumTo_re(num3_re)];
+
+console.log(`With recursion: ${results_re}`);
