22f06dddae7a138ec357e89cb4263c099bf2c9e3
Rinat Valiullov <4rontender@gmail.com>
2021-08-20 23:52:06 +0500

    fix: add temporarily "any" type

--- javascript/junior/deep-copy/deepCopy.ts
+++ javascript/junior/deep-copy/deepCopy.ts
@@ -1,15 +1,16 @@
 import assert from 'assert/strict';
 
+
 const deepCopy = <T>(original: T): T => {
 
   if (Array.isArray(original)) {
-    const copy = [];
+    const copy: any = [];
     for (const [index, value] of original.entries()) {
       copy[index] = deepCopy(value)
     }
     return copy;
   } else if (typeof original === 'object' && original !== null) {
-    const copy = {};
+    const copy: any = {};
     for (const [key, value] of Object.entries(original)) {
       copy[key] = deepCopy(value);
     }
