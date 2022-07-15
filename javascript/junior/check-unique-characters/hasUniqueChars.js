0123d881af8355ea9224b780a6b3a3003a77555b
Rinat Valiullov <4rontender@gmail.com>
2022-07-15 18:41:43 +0500

    feat: add another solution
    - with Set structure

--- javascript/junior/check-unique-characters/hasUniqueChars.js
+++ javascript/junior/check-unique-characters/hasUniqueChars.js
@@ -1,3 +1,5 @@
+/* 1 way */
+
 const isUnique = (inputString) => {
   for (let i = 0; i < inputString.length; i++) {
     const char = inputString[i];
@@ -10,6 +12,7 @@ const isUnique = (inputString) => {
   return true;
 };
 
+console.log("\n1 way with lastIndexOf method:\n");
 const result1 = isUnique("abcdef");
 const result2 = isUnique("123456");
 const result3 = isUnique("abcABC");
@@ -19,5 +22,31 @@ console.log(`"abcdef", ${result1}`);
 console.log(`"123456", ${result2}`);
 console.log(`"abcABC", ${result3}`);
 console.log(`"abcaef", ${result4}`);
+
+/* 2 way */
+
+const isUniqueWithSet = (inputString) => {
+  let chars = new Set();
+
+  for (let i = 0; i < inputString.length; i++) {
+    const char = inputString[i];
+    if (chars.has(char)) {
+      return false;
+    }
+    chars.add(char);
+  }
+  return true;
+};
+
+const result1_S = isUnique("abcdef");
+const result2_S = isUnique("123456");
+const result3_S = isUnique("abcABC");
+const result4_S = isUnique("abcaef");
+
+console.log("\n2 way with Set structure:\n");
+console.log(`"abcdef", ${result1_S}`);
+console.log(`"123456", ${result2_S}`);
+console.log(`"abcABC", ${result3_S}`);
+console.log(`"abcaef", ${result4_S}`);
 
 export { isUnique };
