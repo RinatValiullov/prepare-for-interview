6a58d014c9f9bc9ba12dce006ce558c6ea1b58a2
Rinat Valiullov <4rontender@gmail.com>
2022-07-31 23:27:07 +0500

    feat(rotated-string): add another solution variant

--- javascript/junior/rotated-string/isStringRotated.js
+++ javascript/junior/rotated-string/isStringRotated.js
@@ -1,4 +1,6 @@
-const isStringRotated = (inputString1, inputString2) => {
+/* 1 way */
+
+const isStringRotatedFor = (inputString1, inputString2) => {
   if (inputString1.length !== inputString2.length) {
     return false;
   }
@@ -15,14 +17,34 @@ const isStringRotated = (inputString1, inputString2) => {
 };
 
 const stringSource = "javascript";
-const string1 = "scriptjava";
-const string2 = "iptjavascr";
-const string3 = "tpircsavaj";
+const string1For = "scriptjava";
+const string2For = "iptjavascr";
+const string3For = "tpircsavaj";
 
-const output1 = isStringRotated(stringSource, string1);
-const output2 = isStringRotated(stringSource, string2);
-const output3 = isStringRotated(stringSource, string3);
+const output1For = isStringRotatedFor(stringSource, string1For);
+const output2For = isStringRotatedFor(stringSource, string2For);
+const output3For = isStringRotatedFor(stringSource, string3For);
 
-console.log(output1);
-console.log(output2);
-console.log(output3);
+console.log("1 way", output1For);
+console.log("1 way", output2For);
+console.log("1 way", output3For);
+
+/* 2 way */
+const isStringRotatedDoubleLength = (inputString1, inputString2) => {
+  return (
+    (inputString1 + inputString1).includes(inputString2) &&
+    inputString1.length === inputString2.length
+  );
+};
+
+const string1Double = "scriptjava";
+const string2Double = "iptjavascr";
+const string3Double = "tpircsavaj";
+
+const output1Double = isStringRotatedDoubleLength(stringSource, string1Double);
+const output2Double = isStringRotatedDoubleLength(stringSource, string2Double);
+const output3Double = isStringRotatedDoubleLength(stringSource, string3Double);
+
+console.log("2 way", output1Double);
+console.log("2 way", output2Double);
+console.log("2 way", output3Double);
