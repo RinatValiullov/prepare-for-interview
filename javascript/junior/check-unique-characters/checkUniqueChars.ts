6f5de1272caf62f99fd9ef6dd4f4f8404d45d913
Rinat Valiullov <4rontender@gmail.com>
2022-08-25 21:14:28 +0500

    feat: add ts version of "checkUniqueChars" task

--- javascript/junior/check-unique-characters/checkUniqueChars.ts
+++ javascript/junior/check-unique-characters/checkUniqueChars.ts
@@ -1,17 +1,22 @@
-const checkUniqueChars = (string) => {
-    let chars = new Set();
-    for (const char of string) {
-        if (chars.has(char)) {
-            return false;
-        }
-        chars.add(char);
+export { };
+
+const checkUniqueChars = (string: string): boolean => {
+  let chars = new Set();
+
+  for (const char of string) {
+    if (chars.has(char)) {
+      return false;
     }
-    return true;
+    chars.add(char);
+  }
+  return true
 };
+
 const inputString1 = "4rontender";
 const inputString2 = "coder";
+
 const result1 = checkUniqueChars(inputString1);
 const result2 = checkUniqueChars(inputString2);
+
 console.log(`"${inputString1}": ${result1}`);
 console.log(`"${inputString2}":`, result2);
-export {};
