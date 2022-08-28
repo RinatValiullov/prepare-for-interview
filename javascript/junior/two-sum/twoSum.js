95a24dfadc2d2270aa5dfe6be989ccf46349e7fc
Rinat Valiullov <4rontender@gmail.com>
2022-08-28 17:23:33 +0500

    feat(two-sum): add another solution

--- javascript/junior/two-sum/twoSum.js
+++ javascript/junior/two-sum/twoSum.js
@@ -1,3 +1,4 @@
+/* 1 way */
 const twoSum = (numbers, target) => {
     for (let firstTerm = 0; firstTerm < numbers.length - 1; firstTerm++) {
         const firstElement = numbers[firstTerm];
@@ -9,16 +10,43 @@ const twoSum = (numbers, target) => {
         }
     }
 };
-const numbers1 = [3, 2, 4];
-const target1 = 6;
-const numbers2 = [-2, 1, 7, 3, 10];
-const target2 = 5;
-const numbers3 = [5, 5];
-const target3 = 10;
-const output1 = twoSum(numbers1, target1);
-const output2 = twoSum(numbers2, target2);
-const output3 = twoSum(numbers3, target3);
-console.log(`Elements in the array [${numbers1}] with indices ${output1?.join(' and ')} form a sum equal to ${target1}`);
-console.log(`Elements in the array [${numbers2}] with indices ${output2?.join(' and ')} form a sum equal to ${target2}`);
-console.log(`Elements in the array [${numbers3}] with indices ${output3?.join(' and ')} form a sum equal to ${target3}`);
-export { twoSum };
+const numbers1_1 = [3, 2, 4];
+const target1_1 = 6;
+const numbers2_1 = [-2, 1, 7, 3, 10];
+const target2_1 = 5;
+const numbers3_1 = [5, 5];
+const target3_1 = 10;
+const output1_1 = twoSum(numbers1_1, target1_1);
+const output2_1 = twoSum(numbers2_1, target2_1);
+const output3_1 = twoSum(numbers3_1, target3_1);
+console.log("1 way");
+console.log(`Elements in the array [${numbers1_1}] with indices ${output1_1?.join(" and ")} form a sum equal to ${target1_1}`);
+console.log(`Elements in the array [${numbers2_1}] with indices ${output2_1?.join(" and ")} form a sum equal to ${target2_1}`);
+console.log(`Elements in the array [${numbers3_1}] with indices ${output3_1?.join(" and ")} form a sum equal to ${target3_1}`);
+/* 2 way */
+const twoSumMap = (numbers, target) => {
+    const indexMap = new Map();
+    for (let i = 0; i < numbers.length; i++) {
+        const element = numbers[i];
+        const complement = target - element;
+        const complementIndex = indexMap.get(complement);
+        if (complementIndex != null) {
+            return [complementIndex, i];
+        }
+        indexMap.set(element, i);
+    }
+};
+const numbers1_2 = [3, 2, 4];
+const target1_2 = 6;
+const numbers2_2 = [-2, 1, 7, 3, 10];
+const target2_2 = 5;
+const numbers3_2 = [5, 5];
+const target3_2 = 10;
+const output1_2 = twoSumMap(numbers1_2, target1_2);
+const output2_2 = twoSumMap(numbers2_2, target2_2);
+const output3_2 = twoSumMap(numbers3_2, target3_2);
+console.log("2 way");
+console.log(`Elements in the array [${numbers1_2}] with indices ${output1_2?.join(" and ")} form a sum equal to ${target1_2}`);
+console.log(`Elements in the array [${numbers2_2}] with indices ${output2_2?.join(" and ")} form a sum equal to ${target2_2}`);
+console.log(`Elements in the array [${numbers3_2}] with indices ${output3_2?.join(" and ")} form a sum equal to ${target3_2}`);
+export { twoSum, twoSumMap };
--- javascript/junior/two-sum/twoSum.ts
+++ javascript/junior/two-sum/twoSum.ts
@@ -1,32 +1,97 @@
-const twoSum = (numbers: number[], target: number): number[] | undefined => {
+/* 1 way */
 
+const twoSum = (numbers: number[], target: number): number[] | undefined => {
   for (let firstTerm = 0; firstTerm < numbers.length - 1; firstTerm++) {
     const firstElement = numbers[firstTerm];
-    for (let secondTerm = firstTerm + 1; secondTerm < numbers.length; secondTerm++) {
+    for (
+      let secondTerm = firstTerm + 1;
+      secondTerm < numbers.length;
+      secondTerm++
+    ) {
       const secondElement = numbers[secondTerm];
       if (firstElement + secondElement === target) {
         return [firstTerm, secondTerm];
       }
     }
   }
+};
 
-}
+const numbers1_1 = [3, 2, 4];
+const target1_1 = 6;
 
-const numbers1 = [3, 2, 4];
-const target1 = 6;
+const numbers2_1 = [-2, 1, 7, 3, 10];
+const target2_1 = 5;
 
-const numbers2 = [-2, 1, 7, 3, 10];
-const target2 = 5;
+const numbers3_1 = [5, 5];
+const target3_1 = 10;
 
-const numbers3 = [5, 5];
-const target3 = 10;
+const output1_1 = twoSum(numbers1_1, target1_1);
+const output2_1 = twoSum(numbers2_1, target2_1);
+const output3_1 = twoSum(numbers3_1, target3_1);
 
-const output1 = twoSum(numbers1, target1);
-const output2 = twoSum(numbers2, target2);
-const output3 = twoSum(numbers3, target3);
+console.log("1 way");
+console.log(
+  `Elements in the array [${numbers1_1}] with indices ${output1_1?.join(
+    " and "
+  )} form a sum equal to ${target1_1}`
+);
+console.log(
+  `Elements in the array [${numbers2_1}] with indices ${output2_1?.join(
+    " and "
+  )} form a sum equal to ${target2_1}`
+);
+console.log(
+  `Elements in the array [${numbers3_1}] with indices ${output3_1?.join(
+    " and "
+  )} form a sum equal to ${target3_1}`
+);
 
-console.log(`Elements in the array [${numbers1}] with indices ${output1?.join(' and ')} form a sum equal to ${target1}`);
-console.log(`Elements in the array [${numbers2}] with indices ${output2?.join(' and ')} form a sum equal to ${target2}`);
-console.log(`Elements in the array [${numbers3}] with indices ${output3?.join(' and ')} form a sum equal to ${target3}`);
+/* 2 way */
 
-export { twoSum };
+const twoSumMap = (numbers: number[], target: number): number[] | undefined => {
+  const indexMap = new Map();
+
+  for (let i = 0; i < numbers.length; i++) {
+    const element = numbers[i];
+
+    const complement = target - element;
+    const complementIndex = indexMap.get(complement);
+    if (complementIndex != null) {
+      return [complementIndex, i];
+    }
+
+    indexMap.set(element, i);
+  }
+};
+
+const numbers1_2 = [3, 2, 4];
+const target1_2 = 6;
+
+const numbers2_2 = [-2, 1, 7, 3, 10];
+const target2_2 = 5;
+
+const numbers3_2 = [5, 5];
+const target3_2 = 10;
+
+const output1_2 = twoSumMap(numbers1_2, target1_2);
+const output2_2 = twoSumMap(numbers2_2, target2_2);
+const output3_2 = twoSumMap(numbers3_2, target3_2);
+
+console.log("2 way");
+console.log(
+  `Elements in the array [${numbers1_2}] with indices ${output1_2?.join(
+    " and "
+  )} form a sum equal to ${target1_2}`
+);
+console.log(
+  `Elements in the array [${numbers2_2}] with indices ${output2_2?.join(
+    " and "
+  )} form a sum equal to ${target2_2}`
+);
+console.log(
+  `Elements in the array [${numbers3_2}] with indices ${output3_2?.join(
+    " and "
+  )} form a sum equal to ${target3_2}`
+);
+
+export { twoSum, twoSumMap };
